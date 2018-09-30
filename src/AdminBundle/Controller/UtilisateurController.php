21e291505a34a315dcca5bce7d492dca9ffbbd1c
lee_minho <elkarny.asmaa@gmail.com>
2018-09-30 20:05:08 +0100

    first commit

---
+++ src/AdminBundle/Controller/UtilisateurController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace AdminBundle\Controller;
+
+use AdminBundle\Entity\Utilisateur;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Utilisateur controller.
+ *
+ * @Route("admin/utilisateur")
+ */
+class UtilisateurController extends Controller
+{
+    /**
+     * Lists all utilisateur entities.
+     *
+     * @Route("/", name="admin_utilisateur_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $utilisateurs = $em->getRepository('AdminBundle:Utilisateur')->findAll();
+
+        return $this->render('utilisateur/index.html.twig', array(
+            'utilisateurs' => $utilisateurs,
+        ));
+    }
+
+    /**
+     * Creates a new utilisateur entity.
+     *
+     * @Route("/new", name="admin_utilisateur_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $utilisateur = new Utilisateur();
+        $form = $this->createForm('AdminBundle\Form\UtilisateurType', $utilisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($utilisateur);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_utilisateur_index');
+        }
+
+        return $this->render('utilisateur/new.html.twig', array(
+            'utilisateur' => $utilisateur,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a utilisateur entity.
+     *
+     * @Route("/{id}", name="admin_utilisateur_show")
+     * @Method("GET")
+     */
+    public function showAction(Utilisateur $utilisateur)
+    {
+        $deleteForm = $this->createDeleteForm($utilisateur);
+
+        return $this->render('utilisateur/show.html.twig', array(
+            'utilisateur' => $utilisateur,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing utilisateur entity.
+     *
+     * @Route("/{id}/edit", name="admin_utilisateur_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Utilisateur $utilisateur)
+    {
+        $deleteForm = $this->createDeleteForm($utilisateur);
+        $editForm = $this->createForm('AdminBundle\Form\UtilisateurType', $utilisateur);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_utilisateur_edit', array('id' => $utilisateur->getId()));
+        }
+
+        return $this->render('utilisateur/edit.html.twig', array(
+            'utilisateur' => $utilisateur,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a utilisateur entity.
+     *
+     * @Route("/{id}", name="admin_utilisateur_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Utilisateur $utilisateur)
+    {
+        $form = $this->createDeleteForm($utilisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($utilisateur);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('admin_utilisateur_index');
+    }
+
+    /**
+     * Creates a form to delete a utilisateur entity.
+     *
+     * @param Utilisateur $utilisateur The utilisateur entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Utilisateur $utilisateur)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('admin_utilisateur_delete', array('id' => $utilisateur->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
